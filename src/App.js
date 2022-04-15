baedad6b1446f92c51d9e8fbee46066acac1e1ef
Mahadeb Sen <mahadebsen237@gmail.com>
2022-04-15 21:30:43 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
